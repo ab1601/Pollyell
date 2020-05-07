76f1d8405897dfc761a8803a54b8fbfbdbbd17b3
ab1601 <57144159+ab1601@users.noreply.github.com>
2020-05-07 15:05:06 +0300

    Update Turnierserie.java

--- Turnierserie.java
+++ Turnierserie.java
@@ -2,6 +2,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 //результаты каждой серии тернира
+// не уверена, правильно ли я реализую этот класс
+//условие консультанта преподавателя использовать Collections для получения результата отдельныхсерий турнира
 public class Turnierserie {
 	Map<Integer, Turnier> turnieren;
 	List<Reiter> reiter;
