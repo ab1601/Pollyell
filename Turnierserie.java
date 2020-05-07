ac817a3eb47bb75b7318a54346486fb604868d23
ab1601 <57144159+ab1601@users.noreply.github.com>
2020-05-07 14:46:54 +0300

    Create Turnierserie.java

---
+++ Turnierserie.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Turnierserie {
+	Map<Integer, Turnier> turnieren;
+	List<Reiter> reiter;
+	int limits;
+	int anzahlTurniere;
+	public Turnierserie(Map<Integer, Turnier> turnieren, List<Reiter> reiter, int limits) {
+	
+	}
+	public void startSerie (int anzahlTurniere) {
+		 Map<Integer, Turnier> turnieren = new HashMap<Integer, Turnier>();
+		  for (int i = 1; i <=anzahlTurniere; i++) {
+		   Turnier turnier = new Turnier( reiter , limits); 
+		   turnieren.put(i, turnier);
+
+		  }
+
+	}
+
+}
